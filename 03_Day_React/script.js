2298d76e4e5cceb4d46ed602490b53b37dd627d2
Ranjeet <sranjeet434@gmail.com>
2023-07-10 22:43:08 +0530

    React 03 Day

---
+++ 03_Day_React/script.js
@@ -0,0 +1,36 @@
+/*
+
+   Parcel ->
+  
+  💲HMR - Hot Module Replacement
+  💲Created a server
+  💲parcel will keep tarck of file
+  💲File Watcher Algorithm - C++
+  💲Binding
+  💲Minify
+  💲Cleaning our Code
+  💲Dev and Production Bulid
+  💲Super Fast Bulid algorithm
+  💲Image Optimization 
+  💲Caching Dev will devlopment
+    ✨ Built in 55ms
+    ✨ Built in 8ms
+    Reducing the time
+  💲Compatable with older version of browser
+  💲Compression
+  💲HTTPS on dev npx parcel index.html --https
+  💲Port Number
+  💲Consist Hashing Algorithm  
+  💲Zero Config
+
+*/ 
+
+
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+const heading1=React.createElement("h1",{id:"title"},"heading 1");
+const heading2=React.createElement("h1",{id:"title"},"heading 2");
+const container=React.createElement("div",{id:"container"},[heading1,heading2]);
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(container);
