1d5bd4a8814d08acd398fac11db4767e7f17446b
Ranjeet <sranjeet434@gmail.com>
2023-07-11 23:05:13 +0530

    react prep

--- 04_Day_React/script.js
+++ 04_Day_React/script.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const heading1=React.createElement("h1",{id:"title"},"heading 1");
-const heading2=React.createElement("h1",{id:"title"},"heading 2");
+const heading1=React.createElement("h1",{id:"title",key:"h1"},"heading 1");
+const heading2=React.createElement("h1",{id:"title",key:"h2"},"heading 2");
 const container=React.createElement("div",{id:"container"},[heading1,heading2]);
 const root=ReactDOM.createRoot(document.getElementById('root'));
 root.render(container);
